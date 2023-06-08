d8a8864740e73cfeac29f67e7764a5f67592e0ca
Kamil <windycate03@gmail.com>
2023-06-08 22:26:07 +0300

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('test App', () => {
+  test('render async', async () => {
+
+    screen.debug();
+    const asyncData = await screen.findByText(/delectus aut autem/i);
+    expect(asyncData).toBeInTheDocument();
+    screen.debug();
+
+  });
+
+  test('click event', () => {
+    render(<App />)
+
+    const btn = screen.getByTestId<HTMLButtonElement>(/toggleBtn/i);
+    expect(btn).toBeInTheDocument();
+
+    expect(btn).toHaveStyle({ color: 'red' })
+
+    expect(screen.queryByText('yo')).toBeNull();
+    fireEvent.click(btn)
+    expect(screen.getByText('yo')).toBeInTheDocument();
+    fireEvent.click(btn)
+    expect(screen.queryByText('yo')).toBeNull();
+  })
+
+  test('input event', () => {
+    render(<App />)
+
+    const input = screen.getByTestId<HTMLInputElement>(/input/i);
+    expect(input).toBeInTheDocument();
+    expect(input).toContainHTML('');
+    fireEvent.input(input, { target: { value: '123' } })
+    expect(input).toContainHTML('123')
+
+    userEvent.type(input, '123123')
+    expect(input).toContainHTML('123123')
+  })
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
+interface todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+
 function App() {
+  const [data, setData] = useState<todo[]>([])
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState('');
+
+
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setToggle(state => !state)
+  }
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  const loadTodos = async () => {
+    const response = await axios.get<todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    setData(response.data)
+  }
+
+  useEffect(() => {
+    loadTodos();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>hello world</p>
+        <input value={value} onChange={handleInput} data-testid="input"></input>
+        <button data-testid="toggleBtn" onClick={handleClick} style={{ color: 'red' }}>click</button>
+        {
+          data.map((item) =>
+            <div>
+              {item.title}
+            </div>)
+        }
+        {toggle && <div>
+          <p>yo</p>
+        </div>}
       </header>
     </div>
   );
